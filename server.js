10047e8067e610a4c4ce48c51d3139dd77c6e77a
VipulKod <vipul.kodle@gmail.com>
2020-04-30 19:31:11 +0530

    Files before creating models

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors")
+const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 //Load env vars
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: "./config/config.env" });
 
-const app = express()
+//Connect to db
+connectDB();
 
-app.get('/api/v1/bootcamps', (req, res)=>{
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' })
-})
+const app = express();
 
-const PORT = process.env.PORT || 5000
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+// Mount routers
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+app.use("/api/v1/bootcamps", bootcamps);
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
 
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`unhandledRejection`, (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //Close server and exit process
+    server.close(() => process.exit(1));
+  });
+});
