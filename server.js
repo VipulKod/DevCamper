d7d3e4a987e3e94136f13b9af15736c1c6716e8c
VipulKod <vipul.kodle@gmail.com>
2020-05-01 20:19:40 +0530

    Added custom error handling restructured controller and added slugify

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors")
+const errorHandler = require('./middleware/error')
 const bootcamps = require("./routes/bootcamps");
 const connectDB = require("./config/db");
 
@@ -23,6 +24,9 @@ if (process.env.NODE_ENV === "development") {
 // Mount routers
 
 app.use("/api/v1/bootcamps", bootcamps);
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
